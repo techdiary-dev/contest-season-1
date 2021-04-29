ac65ca9231af30e9eb7180fc7ca2f87112d452f8
Shoaib Sharif <shoaib19j@gmail.com>
2021-04-29 10:58:26 -0700

    countdown clock added

---
+++ components/CountDown.vue
@@ -0,0 +1,66 @@
+<template>
+  <section class="flex justify-center">
+    <div class="flex flex-col mr-4 my-3 items-center w-[120px]"><span
+      class="countdown-bg-gradient p-4 text-7xl font-bold rounded-md text-white w-[120px]">{{
+        day
+      }}</span>Days
+    </div>
+    <div class="flex flex-col mx-4 my-3 items-center"><span
+      class="countdown-bg-gradient p-4 text-7xl font-bold rounded-md text-white w-[120px]">{{
+        hour
+      }}</span>Hours
+    </div>
+    <div class="flex flex-col mx-4 my-3 items-center"><span
+      class="countdown-bg-gradient p-4 text-7xl font-bold rounded-md text-white w-[120px]">{{
+        minute
+      }}</span>Minutes
+    </div>
+    <div class="flex flex-col ml-4 my-3 items-center"><span
+      class="countdown-bg-gradient p-4 text-7xl font-bold rounded-md text-white w-[120px]">{{
+        second
+      }}</span>Seconds
+    </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hour: null,
+      minute: null,
+      second: null,
+      day: null,
+      countDate: new Date("May 31, 2021 00:00:00").getTime()
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.countDown()
+    }, 1000)
+  },
+  methods: {
+    countDown() {
+      const now = new Date().getTime(),
+        gap = this.countDate - now
+
+      const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+      this.day = Math.floor(gap / day);
+      this.hour = Math.floor((gap % day) / hour)
+      this.minute = Math.floor((gap % hour) / minute)
+      this.second = Math.floor((gap % minute) / second);
+    }
+  },
+}
+</script>
+
+<style>
+.countdown-bg-gradient {
+  background: linear-gradient(205.5deg, #321b50 10.82%, rgba(19, 30, 63, 0.88) 84.93%);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,24 @@
 <template>
   <div>
-    <hero />
+    <hero/>
     <div class="w-6/12 mx-auto -mt-12">
-      <maincard />
+      <maincard/>
     </div>
-
+    <count-down/>
     <div class="mx-auto my-10 wrapper">
       <h2 class="text-4xl leading-relaxed font-boshonto">
-        <span class=" text-fuchsia">কনটেস্টে</span> অংশগ্রহণকারী সর্বশেষ<br />
+        <span class=" text-fuchsia">কনটেস্টে</span> অংশগ্রহণকারী সর্বশেষ<br/>
         <span class=" text-[#FFB95C]">আর্টিক্যাল সমূহ</span>
       </h2>
     </div>
 
     <div class="grid grid-cols-3 gap-5 mx-auto my-10 wrapper">
-      <article-card />
-      <article-card />
-      <article-card />
-      <article-card />
-      <article-card />
-      <article-card />
+      <article-card/>
+      <article-card/>
+      <article-card/>
+      <article-card/>
+      <article-card/>
+      <article-card/>
     </div>
 
     <div class="text-center font-boshonto">
@@ -34,16 +34,19 @@
     <div
       class="p-5 mx-auto my-10 prose prose-lg bg-white shadow wrapper max-w-none"
     >
-      <nuxt-content :document="rules" />
+      <nuxt-content :document="rules"/>
     </div>
   </div>
 </template>
 
 <script>
+import ArticleCard from "~/components/article-card";
+
 export default {
-  async asyncData({ $content }) {
+  components: {ArticleCard},
+  async asyncData({$content}) {
     const rules = await $content("", "welcome").fetch();
-    return { rules };
+    return {rules};
   }
 };
 </script>
